a7c00acefd72040b43cabc412a20ba7d2c84d31b
sparshmallow <sparsh.verma2003@icloud.com>
2026-02-12 18:43:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,8 @@ def archive_download(run_id: int):
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
